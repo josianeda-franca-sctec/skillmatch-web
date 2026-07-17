24441b006dba0f67a251d6d56aba1310281dcee3
josianeda-franca-sctec <josiane_da-franca@estudante.sesisenai.org.br>
2026-07-17 15:48:22 -0300

    cria formulario do candidato

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,28 +3,8 @@ import { carregarVagas } from "./dados.js";
 async function iniciarAplicacao() {
     const vagas = await carregarVagas();
 
-    console.log("Vagas carregadas:", vagas);
-    console.log(`Quantidade de vagas: ${vagas.length}`);
-
-    const habilidadesTeste = [
-        "HTML",
-        "CSS",
-        "JavaScript"
-    ];
-
-    vagas.forEach((vaga) => {
-        const percentual = vaga.calcularCompatibilidade(
-            habilidadesTeste
-        );
-
-        const classificacao = vaga.classificarCompatibilidade(
-            percentual
-        );
-
-        console.log(vaga.apresentarResumo());
-        console.log(`Compatibilidade: ${percentual}%`);
-        console.log(`Classificação: ${classificacao}`);
-    });
+    console.log("Aplicação iniciada.");
+    console.log("Vagas disponíveis:", vagas);
 }
 
 iniciarAplicacao();
